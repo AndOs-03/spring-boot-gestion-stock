4bb84e108b5abd028542e5324bee75c45d2cba57
anderson.ouattara <anderson.ouattara@inexa-ci.com>
2023-01-21 13:12:16 +0100

    core: Initialisation du projet

---
+++ src/test/java/com/andos/gestionstock/SpringBootGestionStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andos.gestionstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGestionStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
